724d5de6594124cf4a7d5e462be1bf520cb1d352
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-08-08 20:24:51 +0430

    current weather implemented

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9e7247e920msh6a257d0161ce6d9p1dc957jsn342ffbd007f3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '35a1fc698c4a6dcad4e9decb62e3ea07'
---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Forcast.css'
+
+const Forecast = () => {
+    return (
+        <>
+            <label className='title'>Daily</label>
+        </>
+    )
+};
+
+export default Forecast;
